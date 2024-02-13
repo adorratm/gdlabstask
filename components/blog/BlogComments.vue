553d26f39fedff4fab136fb459ef745cbb001cc3
Emre KILIÇ <emrekilic19983@gmail.com>
2024-02-13 15:22:31 +0300

    update for comments

--- components/blog/BlogComments.vue
+++ components/blog/BlogComments.vue
@@ -1,5 +1,197 @@
 <template>
-    <div>
-        
+  <div>
+    <!-- Comment Container -->
+    <div
+      v-for="(comment, index) in commentList"
+      :key="index"
+      class="flex w-full justify-between bg-base-100 border my-2 rounded-md"
+    >
+      <div class="p-3" :style="{ marginLeft: comment.indentLevel * 20 + 'px' }">
+        <div class="flex gap-3 items-center">
+          <img
+            :src="comment.authorAvatar"
+            class="object-cover w-10 h-10 rounded-full border-2 border-emerald-400 shadow-emerald-400"
+          />
+          <h3 class="font-bold">
+            {{ comment.author }}
+            <br />
+            <span class="text-sm text-gray-400 font-normal"
+              >{{ comment.email }}</span
+            >
+          </h3>
+        </div>
+        <p class="text-gray-600 mt-2" v-html="formatCommentBody(comment.body)">
+        </p>
+        <div class="flex gap-3 items-center">
+          <button @click="likeComment(index)" class="text-right text-blue-500">
+            <svg
+              class="w-6 h-6 text-gray-600"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="2"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+              />
+            </svg>
+            {{ comment.likes }}
+          </button>
+          <button @click="dislikeComment(index)" class="text-right text-blue-500">
+            <svg
+              class="w-6 h-6 text-gray-600"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="2"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+
+      <div class="flex flex-col gap-3 pr-3 py-3">
+        <div>
+          <svg
+            class="w-6 h-6 text-gray-600"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="5"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M4.5 15.75l7.5-7.5 7.5 7.5"
+            />
+          </svg>
+        </div>
+        <div>
+          <svg
+            class="w-6 h-6 text-gray-600"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="5"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+            />
+          </svg>
+        </div>
+      </div>
     </div>
+    <!-- END Comment Container  -->
+
+    <!-- Comment Form -->
+    <div class="w-full px-3 mb-2 mt-6">
+      <textarea
+        v-model="newComment.body"
+        class="bg-gray-100 rounded border border-gray-400 leading-normal resize-none w-full h-20 py-2 px-3 font-medium placeholder-gray-400 focus:outline-none focus:bg-white"
+        name="body"
+        placeholder="Comment"
+        required
+      ></textarea>
+    </div>
+
+    <div class="w-full flex justify-end px-3 my-3">
+      <button
+        @click="postComment"
+        class="px-2.5 py-1.5 rounded-md text-white text-sm bg-indigo-500 text-lg"
+      >
+        Post Comment
+      </button>
+    </div>
+    <!-- END Comment Form -->
+  </div>
 </template>
+
+<script setup lang="ts">
+const props = defineProps({
+  comments: {
+    type: Array,
+    required: true,
+  },
+});
+
+const commentList = ref([...props.comments]);
+
+commentList.value.forEach((comment) => {
+    comment.likes = Math.floor(Math.random() * 100); // Rastgele 0-100 arası beğeni sayısı
+    comment.authorLevel = Math.floor(Math.random() * 10); // Rastgele 0-10 arası seviye
+    comment.authorAvatar = `https://api.dicebear.com/7.x/lorelei/svg?seed=${comment.author}`; // Yorumu gönderen kullanıcının avatar URL'si
+    comment.author = comment.email.split("@")[0]; // Yorumu gönderen kullanıcının adı
+    comment.indentLevel = 0; // Varsayılan girinti seviyesi
+  const randomMinutes = Math.floor(Math.random() * (60 * 24 * 7)); // 7 gün içinde rastgele dakika
+  comment.date = new Date(Date.now() - randomMinutes * 60000); // Şu andan rastgele dakika öncesi
+});
+
+const newComment = ref({ body: "" });
+
+const postComment = async () => {
+  try {
+    // Burada yeni yorumu göndermek için bir HTTP isteği gönderin
+    // Örnek olarak JSONPlaceholder'a bir POST isteği gönderebilirsiniz.
+    console.log("Posting comment:", newComment.value.body);
+    // Başarılı bir şekilde yorum gönderildikten sonra comments dizisine de ekleyebilirsiniz.
+    commentList.value.unshift({
+      body: newComment.value.body,
+      author: "adorratm", // Yorumu gönderen kullanıcının adını buraya ekleyin.
+      authorAvatar: `https://api.dicebear.com/7.x/lorelei/svg?seed=adorratm`, // Kullanıcının avatar URL'sini buraya ekleyin.
+      date: new Date(), // Şu anki zaman
+      indentLevet: 0, // Varsayılan girinti seviyesi
+    });
+    // Yorum gönderildikten sonra metni sıfırlayın
+    newComment.value.body = "";
+  } catch (error) {
+    console.error("Error posting comment:", error);
+  }
+};
+
+const likeComment = (index) => {
+    commentList.value[index].likes++;
+};
+
+const dislikeComment = (index) => {
+  commentList.value[index].likes--;
+};
+
+const deleteComment = (index) => {
+    commentList.value.splice(index, 1);
+};
+
+const editComment = (index) => {
+  // Yorumu düzenleme işlemleri
+};
+
+const replyToComment = (index) => {
+  // Yoruma yanıt verme işlemleri
+  // @ işaretiyle etiketleme
+  newComment.value.body = `@${commentList.value[index].author} `;
+};
+
+const formatCommentBody = (body) => {
+    // Yorumda @ işaretiyle etiketlenen kullanıcıları bulma
+    const explodedBody = body.split(" ");
+    let splittedWord = "";
+    explodedBody.forEach((word, index) => {
+        splittedWord = word.split("@")[0];
+    });
+    const findComment = commentList.value.find((comment) => comment?.email?.split("@")[0] === splittedWord);
+  // @ işaretiyle etiketlenen kullanıcıları yorumda link haline getirme
+  return body.replace(/@(\w+)/g, '<a class="text-primary font-bold" href="'+findComment?.email+'" target="_blank">@$1</a>');
+};
+</script>
--- pages/blogs/[id].vue
+++ pages/blogs/[id].vue
@@ -1,40 +1,65 @@
 <template>
-  <div class="card w-100 bg-base-100 shadow-xl">
-    <figure v-if="post.photos && post.photos.length > 0">
-      <Carousel :items-to-show="1" :autoplay="5000">
-        <Slide :key="index" v-for="(photo, index) in post.photos">
-          <div class="carousel__item w-full">
-            <NuxtImg :src="photo.url" :alt="photo.title" class="w-full" />
+  <div>
+    <div class="card w-100 bg-base-100 shadow-xl">
+      <figure v-if="post.photos && post.photos.length > 0">
+        <Carousel :items-to-show="1" :autoplay="5000">
+          <Slide :key="index" v-for="(photo, index) in post.photos">
+            <div class="carousel__item w-full">
+              <NuxtImg :src="photo.url" :alt="photo.title" class="w-full" />
+            </div>
+          </Slide>
+          <template #addons>
+            <navigation />
+            <pagination class="flex-wrap" />
+          </template>
+        </Carousel>
+      </figure>
+      <div class="card-body">
+        <h2 class="card-title">
+          {{ post.title }}
+        </h2>
+        <p>{{ post.body }}</p>
+        <div class="card-actions items-center border-t-2 pt-2">
+          <div class="flex-1 items-center" v-if="post.user">
+            <div class="flex items-center">
+              <NuxtImg
+                v-if="post.user && post.photos.length"
+                class="w-12 rounded-full border-2 mr-4"
+                :src="'https://api.dicebear.com/7.x/lorelei/svg?seed='+post.user.username"
+                :alt="post.user.username"
+              />
+              <div class="text-sm">
+                <p class="text-gray-900 leading-none">
+                  <NuxtLink target="_blank" :to="'https://' + post.user.website"
+                    >@{{ post.user.username }}</NuxtLink
+                  >
+                </p>
+                <p class="text-gray-600">
+                  <NuxtLink :to="'https://' + post.user.website" target="_blank">{{
+                    post.user.website
+                  }}</NuxtLink>
+                </p>
+              </div>
+            </div>
           </div>
-        </Slide>
-        <template #addons>
-          <navigation />
-          <pagination class="flex-wrap" />
-        </template>
-      </Carousel>
-    </figure>
-    <div class="card-body">
-      <h2 class="card-title">
-        {{ post.title }}
-      </h2>
-      <p>{{ post.body }}</p>
-      <div class="card-actions items-center border-t-2 pt-2">
-        <div class="flex-1 items-center" v-if="post.user">
-          <div class="flex items-center">
-            <NuxtImg v-if="post.user && post.photos.length"
-              class="w-12 rounded-full mr-4"
-              :src="post.photos[0].url"
-              :alt="post.user.username"
-            />
-            <div class="text-sm">
-              <p class="text-gray-900 leading-none"><NuxtLink target="_blank" :to="'https://'+post.user.website">@{{ post.user.username }}</NuxtLink></p>
-              <p class="text-gray-600"><NuxtLink :to="'https://'+post.user.website" target="_blank">{{ post.user.website }}</NuxtLink></p>
+          <div class="justify-end flex-wrap">
+            <div
+              class="badge badge-outline ms-1"
+              v-for="splittedName in post.title?.split(' ')"
+            >
+              <NuxtLink :to="'/blogs/search/' + splittedName">{{
+                splittedName
+              }}</NuxtLink>
             </div>
           </div>
         </div>
-        <div class="justify-end flex-wrap">
-          <div class="badge badge-outline ms-1" v-for="splittedName in post.title?.split(' ')"><NuxtLink :to="'/blogs/search/'+splittedName">{{ splittedName }}</NuxtLink></div>
-        </div>
+      </div>
+    </div>
+
+    <div class="card w-100 bg-base-200 shadow-xl mt-4">
+      <div class="card-body">
+        <h2 class="card-title">Comments</h2>
+        <BlogComments v-if="post.comments" :comments="post.comments" />
       </div>
     </div>
   </div>
